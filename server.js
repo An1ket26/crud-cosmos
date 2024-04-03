f784fd2d94a8eccf6aebff40c73b41bb1f769299
Aniket <01aniket26@gmail.com>
2024-04-03 22:51:17 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use("/deleteemp", EmpController.deleteEmployee)
 app.use("/updateemp",EmpController.updateEmployee)
 
 
-mongoose.connect("mongodb://firstapp:Xbyp3jCh7hVjBsFSytf8GtDZvUfwpcrR2YLvUf0tZflDXfrNnQviX5fPB8NZ9H03j0QAqLo5jvNzACDbfQSBrA==@firstapp.mongo.cosmos.azure.com:10255/Employee?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@firstapp@").then(() => {
+mongoose.connect("connection string").then(() => {
     app.listen(5000, (err) => {
         if (!err) {
             console.log("server running at port 5000");
